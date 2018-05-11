f10ad8982fc1137221993170ad1d93b1db75c220
Soar <578394545@qq.com>
2018-05-11 18:11:41 +0800

    业务添加

---
+++ src/main/java/com/soar/basicframework/insurance/service/SchemeService.java
@@ -0,0 +1,15 @@
+package com.soar.basicframework.insurance.service;
+
+import com.soar.basicframework.base.BaseService;
+import com.soar.basicframework.insurance.model.Scheme;
+
+
+/**
+ * 方案服务层接口
+ * @author Soar
+ * @date 2018/5/9
+ */
+public interface SchemeService extends BaseService<Scheme> {
+
+
+}
---
+++ src/main/java/com/soar/basicframework/insurance/service/SchemeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.soar.basicframework.insurance.service;
+
+import com.soar.basicframework.base.BaseDao;
+import com.soar.basicframework.base.BaseServiceImpl;
+import com.soar.basicframework.insurance.dao.InsuranceDao;
+import com.soar.basicframework.insurance.dao.SchemeDao;
+import com.soar.basicframework.insurance.model.InsuredPersonInformation;
+import com.soar.basicframework.insurance.model.PolicyholderInformation;
+import com.soar.basicframework.insurance.model.Scheme;
+import com.soar.basicframework.utils.GUID;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * 方案服务层实现
+ * @author Soar
+ * @date 2018/5/8
+ */
+@Transactional(rollbackFor = Exception.class)
+@Service
+public class SchemeServiceImpl extends BaseServiceImpl<Scheme> implements SchemeService {
+
+    @Resource
+    private SchemeDao schemeDao;
+
+    /**
+     * 抽象方法，必须实现，返回DAO实例
+     *
+     * @return DAO实例
+     */
+    @Override
+    public BaseDao<Scheme> getDao() {
+        return schemeDao;
+    }
+}
